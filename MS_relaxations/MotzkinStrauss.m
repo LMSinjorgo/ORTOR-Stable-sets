5821f218ede475d5e14d4e3a262c0278348cccd9
LMSinjorgo <94172274+LMSinjorgo@users.noreply.github.com>
2021-11-21 18:12:07 +0100

    Update MotzkinStrauss.m

--- MS_relaxations/MotzkinStrauss.m
+++ MS_relaxations/MotzkinStrauss.m
@@ -1,4 +1,7 @@
 function [alphaG, runTime, x] = MotzkinStrauss(A)
+% MotzkinStrauss formulation
+% A is the adjacency matrix of the graph.
+% Runs for 30 seconds, or less if optimality gap of 0.8% has been reached. (Due to time constraints)
 n = size(A,2);
 Q = A + eye(n);
 x = sdpvar(n,1);
@@ -6,10 +9,6 @@ e = ones(n,1);
 Constraints = [e'*x == 1; x >= 0];
 Objective = x'*Q*x;
 
-%   KKT based indefinite QP solver
-%   is already implemented by matlab
-%   ops= sdpsettings('solver','kktqp');
-%   see also https://yalmip.github.io/example/nonconvexquadraticprogramming/
 ops= sdpsettings('solver','gurobi', 'verbose', 0, 'cachesolvers', 1);
 ops.gurobi.MIPGap = 0.008;
 ops.gurobi.TimeLimit = 30;
@@ -20,8 +19,5 @@ runTime = toc;
 
 x = value(x);
 alphaG = 1 / value(Objective);
-
-
-
 end
 
